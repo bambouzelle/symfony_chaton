66826c91efc97d2679ef3e4e1af0a2ad6d9946db
Felix <felixyeu25@gmail.com>
2021-09-28 12:56:25 +0200

    deuxième part de cours

--- src/Controller/ChatonsController.php
+++ src/Controller/ChatonsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +12,18 @@ class ChatonsController extends AbstractController
     #[Route('', name: 'home')]
     public function index(): Response
     {
-        return $this->render('chatons/index.html.twig', [
-            'controller_name' => 'ChatonsController',
-        ]);
+        // je vais chercher un objet dans le framework symfony
+        //pour lister les dossiers qui sont dans /public/photos
+        $finder = new Finder();
+        $finder->directories()->in("../public/photos");
+        return $this->render('chatons/index.html.twig', ["dossiers"=>$finder]);
+    }
+
+    #[Route('/voir/{dossier}', name: 'voirDossier')]
+    public function voirDossier($dossier){
+        $finder=new Finder();
+        $finder->files()->in("../public/photos/".urldecode($dossier));
+        return $this->render('chatons/voirDossier.html.twig', ["photos"=>$finder, "dossier"=>$dossier]);
+
     }
 }
---
+++ src/Controller/MenuController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MenuController extends AbstractController
+{
+    public function menu(): Response
+    {
+        return $this->render('menu/_menu.html.twig', [
+            'controller_name' => 'MenuController',
+        ]);
+    }
+}
